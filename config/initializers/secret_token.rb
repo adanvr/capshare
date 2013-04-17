57ca43ba2c8c74b8b6fdda5a84ba7e5073f40c09
Adan Romero <adanvr@gmail.com>
2013-04-16 19:48:52 -0700

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Capshare::Application.config.secret_token = 'e9dd7d4e9194e084503f7f431c33ac9b5b05a9bfa1a6e22f0cb2bd447c1bdb89b3d967ce694a45fe336088f6958d1d9f4563383807a81046ded8e9a006014b49'
